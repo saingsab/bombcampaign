852a77d2bb97a00b15f7f5d8028c5832341c1958
Mr.Ayoung <saing.sab@gmail.com>
2018-07-18 10:43:48 +0700

    add mail service

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bombcampaign "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Bombcampaign: Is the newsletters, subscribers and promotions automation mailing service"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.draines/postal "2.0.2"]])
